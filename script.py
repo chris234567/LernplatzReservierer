16502db00cd5e6b29998da74d60be691504ab9ea
unknown <christoph-stade@outlook.de>
2021-06-03 19:38:44 +0200

    changes

--- script.py
+++ script.py
@@ -5,35 +5,14 @@ from time import sleep
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-# TODO: mabe call parameter in externem file speichern (zugangsdaten)
-#       mabe outlook kalender events implementieren fuer bessere planung
+# login data
 
-# id = sys.argv[1] # '05601305875'
-# pwd = sys.argv[2] # '310701x!y?ZZ'
+id = sys.argv[1]
+pwd = sys.argv[2]
 
-# first_name = sys.argv[3] # 'Christoph'
-# last_name = sys.argv[4] # 'Stade'
-# phone_number = sys.argv[5] # '01705971571'
-
-id = '05601305875'
-pwd = '310701x!y?ZZ'
-
-first_name = 'Christoph'
-last_name = 'Stade'
-phone_number = '01705971571'
-
-# AA:  006.A, 007.A, 008.A, 009.A, 010.A, 011.A
-# EG:  001 A, 001 C, 002 A, 002 B, 002 C, 003 A, 003 B, 003 C, 004 A, 004 B, 004 C, 005 B, 005 C
-# OG1: 109 A, 110 C, 111 A, 112 C, 113 A, 114 C, 115 A, 116 C, 117 A, 117 D, 117 H, 119 A, 119 D, 119 F, 120 B, 120 E, 120 H, 121 A, 121 B, 121 C, 121 D, 201 A, 202 C, 203 A, 204 C, 205 A, 206 C, 207 A, 207 G, 208 C, 208 E, 209 A, 209 G, 210 C, 210 E, 211 A, 211 E, 212 C, 212 G, 213 A, 213 E, 214 C, 214 G, 215 A, 215 D
-# CIP: 216 C, 217 A ,218 C, 219 A, 220 C
-# OG2: 301 C, 302 A, 303 C, 304 A, 304 E, 305 A, 305 D, 305 G, 305 J, 306 A, 307 A, 308 A, 311 A, 312 A, 313 A, 314 A, 315 A, 318 A, 320 A, 321 C, 322 A, 323 A, 324 C, 325 A, 326 F, 327 A, 327 D, 327 H, 328 A, 328 B, 328 C, 328 D, 401 A, 402 C, 403 A, 404 C, 405 A, 406 C, 407 A, 409 A, 409 D, 409 G, 410 A, 410 D, 410 G, 412 A, 413 A, 414 C, 414 G, 415 E, 416 A, 416 D, 416 G, 501 C, 501 G, 502 A, 503 C, 503 F, 504 A, 505 C, 506 A, 507 C, 508 A, 509 C, 510 F, 511 F, 602 A, 604 A, 606 A, 607 C, 608 A, 608 G, 609 C, 609 F, 610 C, 610 E, 610 G, 701 A, 702 A,703 A
-
-# floors:
-# hb-og-2
-# hauptbibliothek-1-obergeschoss-cip-pool
-# hb-og1
-# hb-eg-1
-# hb-eg-arbeitsabteile
+first_name = sys.argv[3]
+last_name = sys.argv[4]
+phone_number = sys.argv[5]
 
 # room parameter 
 
@@ -85,5 +64,20 @@ try:
 except:
     sys.stdout.write('Raum ist zu gegebenem Datum und Timeslot nicht verfuegbar.')
 
-while True:
-    pass
+
+# TODO: mabe call parameter in externem file speichern (zugangsdaten)
+#       mabe outlook kalender events implementieren fuer bessere planung
+#       mabe heroku scheduler
+
+# AA:  006.A, 007.A, 008.A, 009.A, 010.A, 011.A
+# EG:  001 A, 001 C, 002 A, 002 B, 002 C, 003 A, 003 B, 003 C, 004 A, 004 B, 004 C, 005 B, 005 C
+# OG1: 109 A, 110 C, 111 A, 112 C, 113 A, 114 C, 115 A, 116 C, 117 A, 117 D, 117 H, 119 A, 119 D, 119 F, 120 B, 120 E, 120 H, 121 A, 121 B, 121 C, 121 D, 201 A, 202 C, 203 A, 204 C, 205 A, 206 C, 207 A, 207 G, 208 C, 208 E, 209 A, 209 G, 210 C, 210 E, 211 A, 211 E, 212 C, 212 G, 213 A, 213 E, 214 C, 214 G, 215 A, 215 D
+# CIP: 216 C, 217 A ,218 C, 219 A, 220 C
+# OG2: 301 C, 302 A, 303 C, 304 A, 304 E, 305 A, 305 D, 305 G, 305 J, 306 A, 307 A, 308 A, 311 A, 312 A, 313 A, 314 A, 315 A, 318 A, 320 A, 321 C, 322 A, 323 A, 324 C, 325 A, 326 F, 327 A, 327 D, 327 H, 328 A, 328 B, 328 C, 328 D, 401 A, 402 C, 403 A, 404 C, 405 A, 406 C, 407 A, 409 A, 409 D, 409 G, 410 A, 410 D, 410 G, 412 A, 413 A, 414 C, 414 G, 415 E, 416 A, 416 D, 416 G, 501 C, 501 G, 502 A, 503 C, 503 F, 504 A, 505 C, 506 A, 507 C, 508 A, 509 C, 510 F, 511 F, 602 A, 604 A, 606 A, 607 C, 608 A, 608 G, 609 C, 609 F, 610 C, 610 E, 610 G, 701 A, 702 A,703 A
+
+# floors:
+# hb-og-2
+# hauptbibliothek-1-obergeschoss-cip-pool
+# hb-og1
+# hb-eg-1
+# hb-eg-arbeitsabteile
